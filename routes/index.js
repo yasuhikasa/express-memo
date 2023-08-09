42d9b6369db9a0499c2a82a63a111069aa0027ec
yasuhikasa <yasuhikasa@yahoo.co.jp>
2023-08-09 23:58:28 +0900

    情報追加

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,14 @@ router.get('/', function(req, res, next) {
 res.render('index', { title: 'Express' });
 });
 
-router.post('/post', (req, res) => {
+router.post('/post', async(req, res) => {
   const customHeader = req.headers['app-type'];
 
   console.log(customHeader);
 
   if (customHeader === 'settings') {
     console.log('success');
-    // res.redirect('http://localhost:3000/receiveHeader'); // Next.jsアプリへリダイレクト
+    res.status(200).json({ message: 'Data received successfully!' });
   } else {
     res.status(403).json({ error: 'Invalid header value' });
   }
@@ -21,3 +21,24 @@ router.post('/post', (req, res) => {
 
 module.exports = router;
 
+// var express = require('express');
+// var router = express.Router();
+
+// // 共通の変数でヘッダー情報を一時保存
+// let customHeader;
+
+// router.post('/receiveFromA', function(req, res) {
+//   customHeader = req.headers['app-type']; // ヘッダー情報を保存
+//   console.log(customHeader);
+//   res.status(200).json({ message: 'Header received from A' });
+// });
+
+// router.get('/sendToB', function(req, res) {
+//   if (customHeader) {
+//     res.status(200).json({ customHeader: customHeader });
+//   } else {
+//     res.status(404).json({ error: 'Header not found' });
+//   }
+// });
+
+// module.exports = router;
