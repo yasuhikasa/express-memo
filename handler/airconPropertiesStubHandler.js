af03f593392491dfa9c1190f1eb894cac7b0f8f8
yasuhikasa <yasuhikasa@yahoo.co.jp>
2023-09-11 19:31:46 +0900

    エアコンプロパティ全件取得

---
+++ handler/airconPropertiesStubHandler.js
@@ -0,0 +1,11 @@
+export function getAirconProperties(req, res) {
+  const { request_type, source_module, request_id } = req.body;
+  
+  if (request_type === 'getAllAirconPropScope' && source_module === 'ApiServer'  && typeof request_id === 'number') {
+    // Stub data
+    const stubData = {/* ここにスタブデータ */};
+    res.json(stubData);
+  } else {
+    res.status(400).json({ message: 'Invalid request for stub' });
+  }
+}
