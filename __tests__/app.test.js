4a05ff728ed5517f31a8030fb1c80870db15e2d8
yasuhikasa <yasuhikasa@yahoo.co.jp>
2023-10-15 21:35:26 +0900

    jestの例追加

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,3 +8,62 @@ describe('GET /', () => {
     expect(response.text).toBe('Hello, world!'); // 期待するレスポンスを指定
   });
 });
+
+
+const { myFunction } = require('../path_to_handler_directory/myHandler');
+
+describe('myFunction', () => {
+  it('should process the input and return the expected result', () => {
+    const input = ...; // 関数の入力
+    const expectedResult = ...; // 期待される出力
+
+    const result = myFunction(input);
+
+    expect(result).toEqual(expectedResult);
+  });
+});
+
+
+
+import { getAirConditionerProperties } from '../path_to_your_handler_file';
+import * as ioRedis from '../path_to_ioRedis_module';
+import * as _ from '../path_to_util_module';
+import { Request, Response } from 'express';
+
+jest.mock('../path_to_ioRedis_module');
+jest.mock('../path_to_util_module');
+
+describe('getAirConditionerProperties', () => {
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: jest.Mock;
+
+  beforeEach(() => {
+    // リクエストとレスポンスオブジェクトのモックをセットアップ
+    req = {};
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    next = jest.fn();
+
+    // Redisの操作とユーティリティ関数のモックをリセット
+    (ioRedis.getRedisInstance as jest.Mock).mockReset();
+    (_.createReqNumber as jest.Mock).mockReset();
+  });
+
+  it('should fetch data from Redis when airconPropertyList is empty', async () => {
+    // Redisとユーティリティ関数のモックの戻り値をセットアップ
+    (ioRedis.getRedisInstance as jest.Mock).mockReturnValue({
+      rpush: jest.fn().mockResolvedValueOnce(true),
+      blpopWithTimeout: jest.fn().mockResolvedValueOnce({ status: 0, aircon_property_scope_list: [] })
+    });
+    (_.createReqNumber as jest.Mock).mockReturnValue('12345');
+
+    await getAirConditionerProperties(req as Request, res as Response, next);
+
+    expect(res.json).toHaveBeenCalledWith({ aircon_property_list: [] });
+  });
+
+  // その他のテストケース...
+});
