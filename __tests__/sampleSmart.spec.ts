36e349897409205df67e1aa1d20a8c4f3b1467b9
yasuhikasa <yasuhikasa@yahoo.co.jp>
2023-10-19 22:15:43 +0900

    テストコード追加

--- __tests__/sampleSmart.spec.ts
+++ __tests__/sampleSmart.spec.ts
@@ -79,3 +79,156 @@ describe('スマートメーターの新規登録', () => {
 		}));
 	});
 });
+
+
+    // 入力フィールドのバリデーション」と「正常応答のハンドリング」
+
+
+// import frisby from 'frisby';
+
+// const baseUrl = 'http://localhost:3000';  // あなたのAPIサーバーのURLに変更してください
+
+// describe('スマートメーターの新規登録', () => {
+
+// 	// 入力フィールドのバリデーション
+// 	describe('入力フィールドのバリデーション', () => {
+
+// 		it('auth_idが欠落している場合、400エラーを返す', (done) => {
+// 			frisby.post(`${baseUrl}/smartmeter/actions/startRegistration`, {
+// 				password: 'sample_password',
+// 				device_name: 'sample_device_name',
+// 				set_type: 1
+// 			})
+// 				.expect('status', 400)
+// 				.expect('json', 'error', 'Invalid properties')  
+// 				.done(done);
+// 		});
+
+// 		it('passwordが欠落している場合、400エラーを返す', (done) => {
+// 			frisby.post(`${baseUrl}/smartmeter/actions/startRegistration`, {
+// 				auth_id: 'sample_auth_id',
+// 				device_name: 'sample_device_name',
+// 				set_type: 1
+// 			})
+// 				.expect('status', 400)
+// 				.expect('json', 'error', 'Invalid properties')  
+// 				.done(done);
+// 		});
+
+// 		it('device_nameが欠落している場合、400エラーを返す', (done) => {
+// 			frisby.post(`${baseUrl}/smartmeter/actions/startRegistration`, {
+// 				auth_id: 'sample_auth_id',
+// 				password: 'sample_password',
+// 				set_type: 1
+// 			})
+// 				.expect('status', 400)
+// 				.expect('json', 'error', 'Invalid properties')  
+// 				.done(done);
+// 		});
+
+// 		it('set_typeが欠落している場合、400エラーを返す', (done) => {
+// 			frisby.post(`${baseUrl}/smartmeter/actions/startRegistration`, {
+// 				auth_id: 'sample_auth_id',
+// 				password: 'sample_password',
+// 				device_name: 'sample_device_name',
+// 			})
+// 				.expect('status', 400)
+// 				.expect('json', 'error', 'Invalid properties')  
+// 				.done(done);
+// 		});
+
+// 	});
+
+// 	// 正常応答のハンドリング
+// 	describe('正常応答のハンドリング', () => {
+
+// 		it('全ての入力フィールドが正しい場合、202を返す', (done) => {
+// 			frisby.post(`${baseUrl}/smartmeter/actions/startRegistration`, {
+// 				auth_id: 'sample_auth_id',
+// 				password: 'sample_password',
+// 				device_name: 'sample_device_name',
+// 				set_type: 1
+// 			})
+// 				.expect('status', 202)
+// 				.expect('json', 'auth_id', 'sample_auth_id')  
+// 				.expect('json', 'device_name', 'sample_device_name')  
+// 				.expect('json', 'set_type', 1)  
+// 				.done(done);
+// 		});
+
+// 	});
+
+// });
+
+
+// Redisへの要求PUSHの確認:
+// Redisに要求が正しくPUSHされているかを確認するためのテストは、モックライブラリを使用してRedisの関数をモックアップする必要があります。
+
+// 1次応答のエラーハンドリング:
+// 1次応答がエラーを返す場合のハンドリングをテストします。
+
+// 2次応答のエラーハンドリング:
+// 2次応答がエラーを返す場合のハンドリングをテストします。
+
+// import frisby from 'frisby';
+// import * as ioRedis from "@/libs/redis";
+
+// jest.mock('@/libs/redis');
+
+// const baseUrl = 'http://localhost:3000'; 
+
+// describe('スマートメーターの新規登録', () => {
+
+// 	// Redisへの要求PUSHの確認
+// 	describe('Redisへの要求PUSH', () => {
+// 		it('正常な要求でRedisにPUSHする', async (done) => {
+// 			const mockRpush = jest.fn();
+// 			(ioRedis.getRedisInstance as jest.Mock).mockReturnValue({ rpush: mockRpush });
+
+// 			await frisby.post(`${baseUrl}/smartmeter/actions/startRegistration`, {
+// 				auth_id: 'sample_auth_id',
+// 				password: 'sample_password',
+// 				device_name: 'sample_device_name',
+// 				set_type: 1
+// 			});
+
+// 			expect(mockRpush).toBeCalled();
+// 			done();
+// 		});
+// 	});
+
+// 	// 1次応答のエラーハンドリング
+// 	describe('1次応答のエラーハンドリング', () => {
+// 		it('1次応答でエラーが返された場合、500エラーを返す', async (done) => {
+// 			(ioRedis.blpopWithTimeout as jest.Mock).mockResolvedValueOnce({ status: 1 });
+
+// 			await frisby.post(`${baseUrl}/smartmeter/actions/startRegistration`, {
+// 				auth_id: 'sample_auth_id',
+// 				password: 'sample_password',
+// 				device_name: 'sample_device_name',
+// 				set_type: 1
+// 			})
+// 				.expect('status', 500)
+// 				.expect('json', 'error', 'Failed to set')
+// 				.done(done);
+// 		});
+// 	});
+
+// 	// 2次応答のエラーハンドリング
+// 	describe('2次応答のエラーハンドリング', () => {
+// 		it('2次応答でエラーが返された場合、500エラーを返す', async (done) => {
+// 			(ioRedis.blpopWithTimeout as jest.Mock).mockResolvedValueOnce({ status: 0 }).mockResolvedValueOnce({ status: 1 });
+
+// 			await frisby.post(`${baseUrl}/smartmeter/actions/startRegistration`, {
+// 				auth_id: 'sample_auth_id',
+// 				password: 'sample_password',
+// 				device_name: 'sample_device_name',
+// 				set_type: 1
+// 			})
+// 				.expect('status', 500)
+// 				.expect('json', 'error', 'Failed to set')
+// 				.done(done);
+// 		});
+// 	});
+
+// });
